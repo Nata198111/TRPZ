46ae4c29fd14602b2046b6fa83aa21f7a873cbce
Nata198111 <shrubovych.nataliia@lll.kpi.ua>
2024-12-04 01:56:12 +0200

    Add lab2

---
+++ src/models/Macro.java
@@ -0,0 +1,35 @@
+package src.models;
+
+
+import java.util.List;
+
+public class Macro {
+    private Long id;
+    private String name;
+    private List<String> commands;
+
+    // Getters and Setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<String> getCommands() {
+        return commands;
+    }
+
+    public void setCommands(List<String> commands) {
+        this.commands = commands;
+    }
+}
---
+++ src/repositories/MacroRepository.java
@@ -0,0 +1,33 @@
+package src.repositories;
+
+
+import src.models.Macro;
+import java.util.ArrayList;
+import java.util.List;
+
+public class MacroRepository implements Repository<Macro> {
+    private List<Macro> macros = new ArrayList<>();
+
+    @Override
+    public void save(Macro macro) {
+        macros.add(macro);
+    }
+
+    @Override
+    public Macro findById(Long id) {
+        return macros.stream()
+                .filter(macro -> macro.getId().equals(id))
+                .findFirst()
+                .orElse(null);
+    }
+
+    @Override
+    public void delete(Long id) {
+        macros.removeIf(macro -> macro.getId().equals(id));
+    }
+
+    @Override
+    public List<Macro> findAll() {
+        return macros;
+    }
+}
